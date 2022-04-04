aca9f01e8f7380a9e49134ea8239f8e356bbf4ca
Le5ya <liliatolstyh@yandex.ru>
2022-04-04 18:45:48 +0300

    fixing hometask

---
+++ src/Components/Banner.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import bannerImg from '../image/banner.jpg';
+
+export const Banner = styled.div`
+width: 100%;
+height: 200px;
+background-image: url(${bannerImg});
+background-position: center;
+background-size: cover;`
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,33 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import dbMenu from './DBMenu';
 import { ListItem } from './ListItems';
-import bunnerImg from '../image/bunner.jpg';
+import { Banner } from './Banner';
+
 
 export const MenuStyled = styled.main`
     background-color: #ccc;
     margin-top: 80px;
 `;
-const Bunner = styled.div`
-    width: 1060px;
-    height: 200px;
-    margin-left: 100px;
-    margin-top: 0;
-    margin-bottom: 30px
-`
-const ImgBunner = styled.img`
-    width: 100%;
-    height: 100%;   
-`;
+
 const SectionMenu = styled.section`
     padding: 30px;
 `
 
 export const Menu = () => (
     <MenuStyled>
-        <Bunner>
-            <ImgBunner src={bunnerImg} alt='logo' />
-
-        </Bunner>
+        <Banner />
         <SectionMenu>
             <h2>Бургеры</h2>
             <ListItem itemList={dbMenu.burger} />
