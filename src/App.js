3d6989c5cba6b7cb54c0eb55e5dacc56a7f76c8e
Le5ya <liliatolstyh@yandex.ru>
2022-04-05 01:04:34 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { NavBar } from './Components/NavBar';
 import { Menu } from './Components/Menu';
 import { GlobalStyle } from './Components/GlobalStyle';
+import { ModalItem } from './Components/ModalItem';
 
 function App() {
+  const [openItem, setOpenItem] = React.useState(null);
+  console.log(openItem);
+
   return (
     <>
-      <NavBar />
-      <Menu />
       <GlobalStyle />
-
+      <NavBar />
+      <Menu setOpenItem={setOpenItem} />
+      <ModalItem openItem={openItem} setOpenItem={setOpenItem} />
     </>
   );
 }
